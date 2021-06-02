5edee6678c5e88de6d418db40006b49ff59b2717
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-02 22:30:36 +0530

    Created a new activity to open web view of recipes

---
+++ app/src/main/java/com/vishnu/recipefinder/activities/ShowLinkActivity.kt
@@ -0,0 +1,12 @@
+package com.vishnu.recipefinder.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vishnu.recipefinder.R
+
+class ShowLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_link)
+    }
+}
