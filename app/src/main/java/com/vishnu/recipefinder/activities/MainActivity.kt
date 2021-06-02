21c2b1f7d8ca848d956221d855b22a36b6eaad20
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-02 20:45:18 +0530

    Setup an onClick Listener to start RecipeList.kt

--- app/src/main/java/com/vishnu/recipefinder/activities/MainActivity.kt
+++ app/src/main/java/com/vishnu/recipefinder/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.vishnu.recipefinder.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vishnu.recipefinder.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchButton.setOnClickListener {
+            startActivity(Intent(this, RecipeList::class.java))
+        }
     }
 }
