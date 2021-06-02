6babd158180676e26852285c12630df176cdf2c6
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-02 22:35:18 +0530

    Implemented WebView onClick of Link button

--- app/src/main/java/com/vishnu/recipefinder/activities/ShowLinkActivity.kt
+++ app/src/main/java/com/vishnu/recipefinder/activities/ShowLinkActivity.kt
@@ -3,10 +3,20 @@ package com.vishnu.recipefinder.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vishnu.recipefinder.R
+import kotlinx.android.synthetic.main.activity_show_link.*
 
 class ShowLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_link)
+
+        //Get extras
+        val extras = intent.extras
+
+        if (extras != null) {
+            val link = extras.get("link")
+            //Invoking WebView
+            webViewId.loadUrl(link.toString())
+        }
     }
 }
