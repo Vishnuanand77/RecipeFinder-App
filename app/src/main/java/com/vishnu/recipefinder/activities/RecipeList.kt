e2b285402f1fb878425829c50afb16659ba0d08d
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-02 20:27:08 +0530

    Initialized a Request Queue

--- app/src/main/java/com/vishnu/recipefinder/activities/RecipeList.kt
+++ app/src/main/java/com/vishnu/recipefinder/activities/RecipeList.kt
@@ -2,11 +2,17 @@ package com.vishnu.recipefinder.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 import com.vishnu.recipefinder.R
 
 class RecipeList : AppCompatActivity() {
+    var volleyRequest: RequestQueue? = null //Initializing a request queue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_list)
+
+        volleyRequest = Volley.newRequestQueue(this) //Assigning a new request queue
     }
 }
