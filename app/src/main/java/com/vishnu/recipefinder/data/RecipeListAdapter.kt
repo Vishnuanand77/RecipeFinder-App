5fb55fa4857cdb180c9e5c1391b21ae0c6269153
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-02 20:22:20 +0530

    Initialized Picasso

--- app/src/main/java/com/vishnu/recipefinder/data/RecipeListAdapter.kt
+++ app/src/main/java/com/vishnu/recipefinder/data/RecipeListAdapter.kt
@@ -1,14 +1,15 @@
 package com.vishnu.recipefinder.data
 
 import android.content.Context
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.vishnu.recipefinder.R
 import com.vishnu.recipefinder.model.Recipe
 
@@ -36,7 +37,25 @@ class RecipeListAdapter(private val list: ArrayList<Recipe>, private val context
         var linkButton = itemView.findViewById<Button>(R.id.recipeLinkButton)
 
         fun bindView(recipe: Recipe) {
+            if (!TextUtils.isEmpty(recipe.thumbnail)) {
+                //Loading thumbnail from remote API into image view on the card.
+                Picasso.get()
+                    .load(recipe.thumbnail)
+                    .placeholder(android.R.drawable.ic_menu_report_image)
+                    .error(android.R.drawable.ic_menu_report_image)
+                    .into(thumbnail)
+            } else {
+                //Load a default image if the API does not have an associated thumbnail
+                Picasso.get().load(R.mipmap.ic_launcher).into(thumbnail)
+            }
+            //TextView Assignments
+            title.text = recipe.title
+            ingredients.text = recipe.ingredients
 
+            //Link Button onClick
+            linkButton.setOnClickListener {
+                TODO("IMPLEMENT LINK OPEN WITH BROWSER FUNCTIONS")
+            }
         }
     }
 }
